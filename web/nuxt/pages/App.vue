5abd43ed1b8e8df1d47e82ffbff25db0e76d03cc
danilovictorio <a17danvicfer@inspedralbes.cat>
2024-02-01 09:05:24 +0100

    crud Front

--- web/nuxt/pages/App.vue
+++ web/nuxt/pages/App.vue
@@ -1,18 +1,69 @@
 <!-- components/App.vue -->
 <template>
-    <div>
-      <h2>¡Bienvenido, {{ username }}!</h2>
-      <p>Has iniciado sesión correctamente.</p>
-    </div>
-  </template>
+  <div>
+    <h2>¡Bienvenido, {{ username }}!</h2>
+    <p>Has iniciado sesión correctamente.</p>
+    <ul>
+      <li v-for="cancion in canciones" :key="cancion.id">
+        <h2>{{ cancion.nombre }}</h2>
+        <p>{{ cancion.artista }}</p>
+        <a :href="cancion.url">Escuchar</a>
+      </li>
+    </ul>
+  </div>
+</template>
   
-  <script>
-  export default {
-    props: ['username'],
-  };
-  </script>
+<script>
+export default {
+  props: ['username'],
+  data() {
+    return {
+      canciones: [],
+      ruta: 'http://localhost:8000',
+    };
+
+  },
+  methods: {
+    async mostrarCanciones() {
+      try {
+
+        const response = await fetch(`${this.ruta}/api/mostrar-canciones`, {
+          method: 'GET',
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+
+        const data = await response.json();
+        this.canciones = data.canciones;
+        console.log(data.message);
+
+        console.log("Canciones mostradas");
+      } catch (error) {
+        console.error('Error al mostrar ocupantes:', error);
+        throw error;
+      }
+    },
+  },
+  async created() {
+    try {
+      const response = await fetch(`/static/canciones.json`);
+
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status}`);
+      }
+
+      const data = await response.json();
+      this.canciones = data.canciones;
+    } catch (error) {
+      console.error('Error al obtener las canciones:', error);
+    }
+  },
+};
+</script>
   
-  <style scoped>
-  /* Puedes agregar estilos específicos para este componente si es necesario */
-  </style>
+<style scoped>
+/* Puedes agregar estilos específicos para este componente si es necesario */
+</style>
   
