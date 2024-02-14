e0a8e05956f712fd8bce903a81de773ebcf34abd
galad22 <a19galdelred@inspedralbes.cat>
2024-02-14 10:39:59 +0100

    api

--- back/laravel/routes/api.php
+++ back/laravel/routes/api.php
@@ -64,7 +64,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register', [AuthController::class, 'register']);
+
 
 
 
